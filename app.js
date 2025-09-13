b8c6c781d2ba67aaf79f84c8d6c4d6c792558b99
Tsuny <contatovitordecastro@gmail.com>
2025-09-13 11:05:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function mensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', `Escolha um número entre 1 e numeroLimite`);
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}`);
 }
 
 mensagemInicial();
@@ -65,4 +65,5 @@ function reiniciarJogo() {
     document.getElementById('reiniciar').setAttribute('disabled', true);
 
 }
+
 
